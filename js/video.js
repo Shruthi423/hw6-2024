1be69cbe4ce412ac496e9b5a038f7e33bdb44fb3
Shruthi <83963935+Shruthi423@users.noreply.github.com>
2024-11-24 17:12:28 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -9,18 +9,58 @@ window.addEventListener("load", function() {
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	video.play()
+	video.play();
 	console.log("Play Video");
 	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 	console.log("volume is updated");
 });
 
 document.querySelector("#pause").addEventListener("click", function () {
-	video.pause()
+	video.pause();
 	console.log("video is paused")
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("video speed is " + video.playbackRate.toFixed(5));
+});
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("video speed is " + video.playbackRate.toFixed(5));
+});
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 10;
+	console.log("video is at " + video.currentTime);
+	if (video.currentTime > video.duration) {
+		video.currentTime = 0;
+		console.log("video is at " + video.currentTime);
+	}
+});
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted){
+		video.muted = false;
+		this.innerHTML = "Mute";
+	console.log("Video unmuted");
+	} else {
+		video.muted = true;
+		this.innerHTML = "Unmute";
+		console.log("Video muted");
+	}
+});
 
+document.querySelector("#slider").addEventListener("input", function() {
+    video.volume = this.value / 100;
+    document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+    console.log("Volume is " + video.volume * 100 + "%");
+});
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+	console.log("video is in vintage mode");
+});
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+    console.log("video is in original mode");
+});
 
 
 // document.querySelector("#play").addEventListener("click", function() {
